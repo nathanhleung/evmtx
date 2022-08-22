8dcbfd0b061f3f3dbc4caa39e24808937fb10fd1
Hao Wang <haowang@Haos-MBP-2.attlocal.net>
2022-08-22 14:47:17 -0700

    resolve merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
 
-import axios from "axios";
-import { ethers } from "ethers";
-import React, { useState } from "react";
+
+
 import SubmitTx from "./components/SubmitTx";
 import { TraceProps } from "./components/Trace";
 import TraceBoard from "./components/TraceBoard";
@@ -19,62 +18,10 @@ const traces: TraceProps[] = [
 ];
 
 export default function App() {
-  const [transactionData, setTransactionData] = useState("");
-  const [value, setValue] = useState("");
-  const [toAddress, setToAddress] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const data = new FormData();
-    data.append("to", toAddress);
-    data.append("value", value);
-    data.append("data", transactionData);
-    axios.post("/sendTxn", data);
-
-    setTimeout(() => setLoading(false), 1000);
-  };
-
   return (
     <div className="App">
       <h1>FIP</h1>
-      <form onSubmit={handleSubmit}>
-        <p>Trace Ethereum transactions with Foundry</p>
-        <br />
-        <span>To Address&nbsp;&nbsp;</span>
-        <input
-          value={toAddress}
-          placeholder="To address"
-          onChange={(e) => setToAddress(e.target.value)}
-        />
-        <br />
-        <span>Value&nbsp;&nbsp;</span>
-        <input
-          value={value}
-          placeholder="Value (wei)"
-          onChange={(e) => setValue(e.target.value)}
-          type="number"
-          style={{ width: "50%" }}
-        />
-        <button
-          type="button"
-          onClick={() => setValue(ethers.utils.parseEther(value).toString())}
-        >
-          To wei
-        </button>
-        <textarea
-          placeholder="Transaction hex data"
-          value={transactionData}
-          onChange={(e) => setTransactionData(e.target.value)}
-          style={{ width: "100%", fontSize: "1.5rem" }}
-          rows={10}
-        />
-        <br />
-        <button type="submit" disabled={loading}>
-          {loading ? "Tracing..." : "Trace"}
-        </button>
-      </form>
+      <SubmitTx/>
       <h2>Trace Data</h2>
       <Transaction
         from="haowang.eth"
@@ -93,7 +40,6 @@ export default function App() {
         <h3> Traces </h3>
         <TraceBoard traces={traces}></TraceBoard>
         <TraceBoard traces={traces}></TraceBoard>
-        <SubmitTx />
       </div>
     </div>
   );
--- frontend/src/components/SubmitTx.tsx
+++ frontend/src/components/SubmitTx.tsx
@@ -1,40 +1,84 @@
-type TransactionProp = {
-    address: string;
-    inputData: string;
-
-}
-
+import axios from "axios";
+import { ethers } from "ethers";
+import React, { useState } from "react";
 
 export default function SubmitTx() {
+    const [transactionData, setTransactionData] = useState("");
+    const [value, setValue] = useState("");
+    const [toAddress, setToAddress] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const data = new FormData();
+        data.append("to", toAddress);
+        data.append("value", value);
+        data.append("data", transactionData);
+        axios.post("/sendTxn", data);
+
+        setTimeout(() => setLoading(false), 1000);
+    };
     return (
         <div className="SubmitTx">
-            <form>
-                <label>
-                    Address:
-                    <input type="text" name="address"/>
-                </label>  <br></br>
-                <label>
-                    Input Data:
-                    <textarea name="inputData" />
-                </label> <br></br>
-                <label>
-                    From :
-                    <input type="text" name="from" placeholder="0x0000000000000000000000000000000000000000" defaultValue="0x0000000000000000000000000000000000000000"/>
-                </label> <br></br>
-                <label>
-                    Gas:
-                    <input type="number" name="Gas" placeholder="0" defaultValue={0} />
-                </label> <br></br>
-                <label>
-                    Gas Price:
-                    <input type="number" name="GasPrice" placeholder="0" defaultValue={0} />
-                </label> <br></br>
-                <label>
-                    Value:
-                    <input type="number" name="Value" placeholder="0" defaultValue={0} />
-                </label>
-                <br></br>
-                <input type="submit" value="Submit" />
+            <p>Trace Ethereum transactions with Foundry</p>
+            <form onSubmit={handleSubmit}>
+                <br />
+                <span>To Address&nbsp;&nbsp;</span>
+                <input
+                    value={toAddress}
+                    placeholder="To address"
+                    onChange={(e) => setToAddress(e.target.value)}
+                />
+                <br />
+                <span>Value&nbsp;&nbsp;</span>
+                <input
+                    value={value}
+                    placeholder="Value (wei)"
+                    onChange={(e) => setValue(e.target.value)}
+                    type="number"
+                    style={{ width: "50%" }}
+                />
+                <br />
+                <span>GasPrice&nbsp;&nbsp;</span>
+                <input
+                    value={value}
+                    placeholder="GasPrice (wei)"
+                    onChange={(e) => setValue(e.target.value)}
+                    type="number"
+                    style={{ width: "50%" }}
+                />
+                <br/>
+                <span>GasLimit&nbsp;&nbsp;</span>
+                <input
+                    value={value}
+                    placeholder="GasLimit (wei)"
+                    onChange={(e) => setValue(e.target.value)}
+                    type="number"
+                    style={{ width: "50%" }}
+                />
+                <br/>
+                <span>From Address&nbsp;&nbsp;</span>
+                <input
+                    value={value}
+                    placeholder="From Address"
+                    onChange={(e) => setValue(e.target.value)}
+                    type="string"
+                    defaultValue="0x0000000000000000000000000000000000000000"
+                    style={{ width: "50%" }}
+                />
+                <br/>
+                <textarea
+                    placeholder="Transaction hex data"
+                    value={transactionData}
+                    onChange={(e) => setTransactionData(e.target.value)}
+                    style={{ width: "100%", fontSize: "1.5rem" }}
+                    rows={10}
+                />
+                <br />
+                <button type="submit" disabled={loading}>
+                    {loading ? "Tracing..." : "Trace"}
+                </button>
             </form>
         </div>
     )
