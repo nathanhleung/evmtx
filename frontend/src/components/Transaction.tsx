b6652c3fa42e2c37886004f33f3a1b0d76fc038f
Hao Wang <haowang@Haos-MBP-2.attlocal.net>
2022-08-22 14:27:24 -0700

    add txn submit

--- frontend/src/components/Transaction.tsx
+++ frontend/src/components/Transaction.tsx
@@ -1,7 +1,7 @@
-type TransactionProp = {
+type TransactionResultProp = {
     exeStatus: boolean;
     from: string;
-    to:string;
+    to: string;
     gasPrice: number;
     value: number;
     transactionFee: number;
@@ -12,21 +12,23 @@ type TransactionProp = {
     inputData: string;
 
 }
-export default function Transaction(props: TransactionProp) {
-    const msg = props.exeStatus ? "Succeeded" : "Failed";
+export default function Transaction({ exeStatus,
+    from, to, gasPrice, value, transactionFee, maxPriorityFeePerGas, maxFeePerGas,
+    gasUsage, gasLimit, inputData}: TransactionResultProp) {
+    const msg = exeStatus ? "Succeeded" : "Failed";
     return (
-      <div className="Transaction" style={{paddingLeft: 20}} >
-        <p> status: {msg} </p>
-        <p> From : {props.from} </p>
-        <p> To : {props.to} </p>
-        <p> Value : {props.value} </p>
-        <p> Transacton Fee : {props.transactionFee} </p>
-        <p> Gas Price : {props.gasPrice} </p>
-        <p> Max Fee Per Gas : {props.maxFeePerGas} </p>
-        <p> Max Priority Fee Per Gas : {props.maxPriorityFeePerGas} </p>
-        <p> Gas Usage : {props.gasUsage} </p>
-        <p> Gas Limit : {props.gasLimit} </p>
-        <p> Input Data : {props.inputData} </p>
-      </div>
+        <div className="Transaction" style={{ paddingLeft: 20 }} >
+            <p> status: {msg} </p>
+            <p> From : {from} </p>
+            <p> To : {to} </p>
+            <p> Value : {value} </p>
+            <p> Transacton Fee : {transactionFee} </p>
+            <p> Gas Price : {gasPrice} </p>
+            <p> Max Fee Per Gas : {maxFeePerGas} </p>
+            <p> Max Priority Fee Per Gas : {maxPriorityFeePerGas} </p>
+            <p> Gas Usage : {gasUsage} </p>
+            <p> Gas Limit : {gasLimit} </p>
+            <p> Input Data : {inputData} </p>
+        </div>
     )
-  }
+}
