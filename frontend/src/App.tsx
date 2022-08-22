daded493f8950b1b331ecd0ae9d7f3d62a08dd99
Hao Wang <haowang@Haos-MBP-2.attlocal.net>
2022-08-22 16:37:44 -0700

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,18 @@
 import "./App.css";
-
-import Connection from "./components/Connection";
-import SubmitTx from "./components/SubmitTx";
-import { TraceProps } from "./components/Trace";
-import TraceBoard from "./components/TraceBoard";
-import Transaction from "./components/Transaction";
-
-const traces: TraceProps[] = [
-  {
-    from: "haowang.eth",
-    to: "nathan.eth",
-    identation: 1,
-  },
-  { from: "nathan.eth", to: "haowang.eth", identation: 2 },
-  { from: "nathan.eth", to: "haowang.eth", identation: 1 },
-];
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
       <h1>FIP</h1>
-      <Connection />
-      <SubmitTx />
-      <h2>Trace Data</h2>
-      <Transaction
-        from="haowang.eth"
-        to="nathan.eth"
-        gasPrice={10}
-        value={1e7}
-        transactionFee={1e10}
-        maxFeePerGas={1}
-        maxPriorityFeePerGas={0}
-        gasLimit={1e7}
-        gasUsage={1e6}
-        inputData="0x"
-        exeStatus={true}
-      ></Transaction>
-      <div>
-        <h3> Traces </h3>
-        <TraceBoard traces={traces}></TraceBoard>
-        <TraceBoard traces={traces}></TraceBoard>
-      </div>
+      <Link to="/test"> <button style={{float:"right"}}> Add a New Transaction </button> </Link>
+      <table>
+        <tr>
+          <th>Transaction Index</th>
+          <th>From</th>
+          <th>To</th>
+        </tr>
+      </table>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,20 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <Routes>
+    <Route path='/' element={<App/>}>  </Route>
+    <Route path='/test' element={<Test/>}> </Route>
+    </Routes>
+    </Router>
   </React.StrictMode>
 )
