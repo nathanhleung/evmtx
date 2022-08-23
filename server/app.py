a6cf5be3d08a578feeac547db77b9308ee03c2b0
Hao Wang <haowang@Haos-MBP-2.attlocal.net>
2022-08-23 15:13:15 -0700

    clean server code

--- server/app.py
+++ server/app.py
@@ -61,71 +61,29 @@ def sendTransaction():
         "data": request.form["data"]
     }
     hexbytes = local_w3.eth.send_transaction(calldata)
-    
-    stateDiff = local_w3.manager.request_blocking("anvil_dumpStateJson", [])
 
-    
+    traceResults = sendDump(calldata, os.getenv("BLOCK_NUMBER"))
     response = jsonify({
         "hexbytes": hexbytes.hex(),
+        "traceResults": traceResults
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 
-def sendDump():
-    """
-    override = 
-      balance
-      nonce
-      code
-      state
-      stateDiff
-    pass in:
-      block_num 
-      call_args
-      override
-    """
-    # get dump
-    w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545/'))
-    dump = w3.manager.request_blocking("anvil_dumpStateJson", [])
-    print(dump)
-    # connect to geth
-    w3 = Web3(Web3.HTTPProvider("http://142.132.152.124:8546"))
-    w3.middleware_onion.inject(web3.middleware.geth_poa_middleware, layer=0)
-    assert not GethDev.w3.isConnected(), "Not connected"
-    # construct call args, block num or hash, trace config, override
-    w3.eth.default_account = w3.toChecksumAddress(
-        "0x70997970c51812dc3a010c7d01b50e0d17dc79c8")
-    # calldata = {
-    #     "to": w3.toChecksumAddress(request.form["to"]),
-    #     "from": w3.eth.default_account,
-    #     "value": request.form["value"]
-    # }
-    # get the parent block that we've used
-    #block_n_hash = os.environ["BLOCK_NUM"]
-    #block_n_hash = request.form["block_num"]
+def sendDump(txData, block):
+    dump = local_w3.manager.request_blocking("anvil_dumpStateJson", [])
     tracer = ""
     with open('tracer.txt') as f:
-      tracer = f.readlines()
-    config = {
-        #"stateoverrides": request.form["override"],
-        "tracer": tracer
-    }
-    #print(tracer)
-    ## send state changes back with debug_traceCall to alchemy
-
-    # trace_result = w3.manager.request_blocking(
-    #     "debug_traceCall",
-    #     [calldata, block_n_hash, config],
-    # )
-    # response = jsonify({
-    #   "trace": trace_result,
-    # })
-    response = jsonify({
-      "trace": "hi"
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+        tracer = f.readlines()
+    trace_result = debug_w3.manager.request_blocking(
+        "debug_traceCall",
+        [txData, block, {
+            "stateoverrides": dump,
+            "tracer": tracer
+        }],
+    )
+    return trace_result
 
 
 @app.route("/getTrace", methods=['POST'])
