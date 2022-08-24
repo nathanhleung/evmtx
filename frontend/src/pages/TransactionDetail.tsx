c428eec7f249a73dd0fdbb1410d22bb02a8ee006
emmaguo13 <emmaguo@berkeley.edu>
2022-08-24 11:29:41 -0700

    remove hardcoding

--- frontend/src/pages/TransactionDetail.tsx
+++ frontend/src/pages/TransactionDetail.tsx
@@ -5,6 +5,7 @@ import { Box } from "@chakra-ui/react";
 import { TraceBoard, Transaction } from "../components/";
 import { TraceProps } from "../components/Trace";
 import { TransactionResultProp } from "../components/Transaction";
+import { collapseTextChangeRangesAcrossMultipleVersions, textChangeRangeIsUnchanged } from "typescript";
 
 type Trace = {
   from: string;
@@ -15,7 +16,7 @@ type Trace = {
 
 export default function TransactionDetail() {
   const { transactionId } = useParams();
-  const [traces, setTraces] = useState<Trace[]>();
+  const [traces, setTraces] = useState<Trace[]>([] as Trace[]);
   const [transactionData, setTransactionData] = useState();
 
   useEffect(() => {
@@ -24,38 +25,42 @@ export default function TransactionDetail() {
       .then((response) => {
         console.log(response.data);
         setTraces(response.data.traces);
+        console.log(traces)
       });
   }, [transactionId]);
-  const txnResult: TransactionResultProp = {
-    exeStatus: true,
-    from: "0xCB2975c5109212fbdCb169a79Eb6eCc84F5D64e3",
-    to: "0xCB2975c5109212fbdCb169a79Eb6eCc84F5D64e3",
-    gasPrice: 0,
-    value: 0,
-    maxPriorityFeePerGas: 0,
-    maxFeePerGas: 0,
-    gasLimit: 0,
-    gasUsage: 0,
-    inputData: "",
-    transactionFee: 0,
-  };
+    const txnResults: TransactionResultProp[] = traces.map(trace => ( {
+      exeStatus: true,
+      from: trace.from,
+      to: trace.to,
+      gasPrice: 0,
+      value: 0,
+      maxPriorityFeePerGas: 0,
+      maxFeePerGas: 0,
+      gasLimit: 0,
+      gasUsage: 0,
+      inputData: "",
+      transactionFee: 0,
+    } as TransactionResultProp));
 
   return (
     <div>
       <TraceBoard traces={traces ?? []} />
-      <Transaction
-        exeStatus={txnResult.exeStatus}
-        from={txnResult.from}
-        to={txnResult.to}
-        gasPrice={txnResult.gasPrice}
-        value={txnResult.value}
-        transactionFee={txnResult.gasPrice * txnResult.gasUsage}
-        maxPriorityFeePerGas={txnResult.maxPriorityFeePerGas}
-        maxFeePerGas={txnResult.maxFeePerGas}
-        gasUsage={txnResult.gasUsage}
-        gasLimit={txnResult.gasLimit}
-        inputData={txnResult.inputData}
-      />
+      {txnResults.map(result => 
+        <Transaction
+          exeStatus={result.exeStatus}
+          from={result.from}
+          to={result.to}
+          gasPrice={result.gasPrice}
+          value={result.value}
+          transactionFee={result.gasPrice * result.gasUsage}
+          maxPriorityFeePerGas={result.maxPriorityFeePerGas}
+          maxFeePerGas={result.maxFeePerGas}
+          gasUsage={result.gasUsage}
+          gasLimit={result.gasLimit}
+          inputData={result.inputData}
+        />
+      )
+      }
       <Box mt={4}>
         <pre>{JSON.stringify(traces, null, 2)}</pre>
       </Box>
