57e8629ac6bbf7768524ad49da68294a9862e9f3
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2022-08-25 13:42:34 -0700

    　prettify new transaction UI

--- frontend/src/pages/NewTransaction.tsx
+++ frontend/src/pages/NewTransaction.tsx
@@ -1,6 +1,6 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import {
   Button,
   FormControl,
@@ -9,61 +9,57 @@ import {
   Input,
   Text,
   Textarea,
-  VStack,
-} from "@chakra-ui/react";
+  VStack
+} from "@chakra-ui/react"
 
 export default function NewTransaction() {
-  const [transactionData, setTransactionData] = useState("0x");
-  const [value, setValue] = useState("");
-  const [toAddress, setToAddress] = useState(
-    "0x0000000000000000000000000000000000000000"
-  );
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [fromAddress, setFromAddress] = useState(
-    "0x0000000000000000000000000000000000000000"
-  );
-  const [gasPrice, setGasPrice] = useState("");
-  const [gasLimit, setGasLimit] = useState("");
-  const navigate = useNavigate();
+  const [transactionData, setTransactionData] = useState("")
+  const [value, setValue] = useState("")
+  const [toAddress, setToAddress] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  const [fromAddress, setFromAddress] = useState("")
+  const [gasPrice, setGasPrice] = useState("")
+  const [gasLimit, setGasLimit] = useState("")
+  const navigate = useNavigate()
 
   async function getGasPrice() {
     const response = await axios.get(
       process.env.REACT_APP_SERVER_URL + "/gas-price"
-    );
-    setGasPrice(response.data.gasPrice);
+    )
+    setGasPrice(response.data.gasPrice)
   }
 
   useEffect(() => {
-    getGasPrice();
-  }, []);
+    getGasPrice()
+  }, [])
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError("");
-    const data = new FormData();
-    data.append("to", toAddress);
-    data.append("value", value);
-    data.append("data", transactionData);
-    data.append("from", fromAddress);
-    data.append("gasPrice", gasPrice);
-    data.append("gasLimit", gasLimit);
+    e.preventDefault()
+    setLoading(true)
+    setError("")
+    const data = new FormData()
+    data.append("to", toAddress)
+    data.append("value", value)
+    data.append("data", transactionData)
+    data.append("from", fromAddress)
+    data.append("gasPrice", gasPrice)
+    data.append("gasLimit", gasLimit)
     try {
       const result = await axios.post(
         process.env.REACT_APP_SERVER_URL + "/transactions/new",
         data
-      );
-      console.log(result.data);
-      console.log(JSON.stringify(result.data));
-      navigate(`/transactions/${result.data.txIndex}`);
+      )
+      console.log(result.data)
+      console.log(JSON.stringify(result.data))
+      navigate(`/transactions/${result.data.txIndex}`)
     } catch (e) {
-      console.error(e);
-      setError((e as any)?.response?.data);
+      console.error(e)
+      setError((e as any)?.response?.data)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
   return (
     <div
       className="Transaction bg-zinc-200 text-black font-semibold py-2 rounded-lg"
@@ -78,7 +74,7 @@ export default function NewTransaction() {
                 background="white"
                 color="black"
                 value={fromAddress}
-                placeholder="From Address"
+                placeholder="0x0000000000000000000000000000000000000000"
                 onChange={(e) => setFromAddress(e.target.value)}
                 type="string"
               />
@@ -89,51 +85,29 @@ export default function NewTransaction() {
                 background="white"
                 color="black"
                 value={toAddress}
-                placeholder="To address"
+                placeholder="0x0000000000000000000000000000000000000000"
                 onChange={(e) => setToAddress(e.target.value)}
               />
             </FormControl>
           </HStack>
           <FormControl flex={1}>
-            <FormLabel>Value</FormLabel>
+            <FormLabel>Value (wei)</FormLabel>
             <Input
               background="white"
               color="black"
               value={value}
-              placeholder="Value (wei)"
+              placeholder="4234789"
               onChange={(e) => setValue(e.target.value)}
               type="number"
             />
           </FormControl>
-          {/* <FormControl className="py-1" flex={1}>
-            <FormLabel>Gas Price (gwei)</FormLabel>
-            <Input
-              background="white"
-              color="black"
-              value={gasPrice}
-              placeholder="GasPrice (gwei)"
-              onChange={(e) => setGasPrice(e.target.value)}
-              type="number"
-            />
-          </FormControl>
-          <FormControl className="py-2" flex={1}>
-            <FormLabel>Gas Limit (gwei)</FormLabel>
-            <Input
-              background="white"
-              color="black"
-              value={gasLimit}
-              placeholder="Gas Limit (gwei)"
-              onChange={(e) => setGasLimit(e.target.value)}
-              type="number"
-            />
-          </FormControl> */}
           <FormControl>
             <FormLabel>Transaction Hex Data</FormLabel>
             <Textarea
               background="white"
               color="black"
               className="rounded-lg"
-              placeholder="Transaction Hex Data"
+              placeholder="0x"
               value={transactionData}
               onChange={(e) => setTransactionData(e.target.value)}
               style={{ width: "100%" }}
@@ -149,5 +123,5 @@ export default function NewTransaction() {
         </Text>
       </form>
     </div>
-  );
+  )
 }
